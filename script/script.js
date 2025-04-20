6217f3ccb17d6472f1c38cf0a8c570b1c51290b0
Manuela Lacerda <mhlacerda.dev@gmail.com>
2025-04-20 17:26:43 -0300

    20/04 - commit de casa

--- script/script.js
+++ script/script.js
@@ -1,24 +1,38 @@
 const btnLogo = document.querySelector(".logo");
-const menu = document.querySelector(".menu__conteudo");
-const btnFolha = document.querySelector(".fa-seedling");
-
-btnLogo.addEventListener ("click", ()=>{
-  if (window.innerWidth < 940){
-    menu.classList.toggle("escondido_menu");
-  } 
-});
-
-btnFolha.addEventListener ("click", ()=>{
-  if (window,innerWidth >= 940){
-    menu.classList.toggle("escondido_menu");
-  }
-});
-
 const itensMenu = document.querySelectorAll(".menu__conteudo li");
-const linkMenu = document.querySelector(".menu__texto");
+const btnFolha = document.querySelector(".fa-seedling");
+const menu = document.querySelector(".menu");
+const menuFlutuante = document.querySelector(".menu-flutuante");
 
-itensMenu.forEach(linkMenu => {
-  linkMenu.addEventListener("click", ()=>{
-    menu.classList.toggle("escondido_menu");
+// btnLogo.addEventListener ("click", ()=>{
+//   if (window.innerWidth < 940){
+//     menu.classList.toggle("escondido");
+//   } 
+// });
+
+// btnFolha.addEventListener ("click", ()=>{
+//   if (window,innerWidth >= 940){
+//     menu.classList.toggle("escondido");
+//   }
+// });
+
+btnLogo.addEventListener('click', () => {
+  menu.classList.toggle('ativo');
+});
+
+btnFolha.addEventListener('click', () => {
+  menu.classList.toggle('ativo');
+});
+
+menuFlutuante.addEventListener("click", () =>{
+  menu.classList.toggle('ativo');
+});
+
+const linkMenu = document.querySelector(".menu__texto"); /*são as li*/
+
+itensMenu.forEach(item => {
+  item.addEventListener("click", ()=>{
+    menu.classList.remove("ativo");
   });
 });
+
